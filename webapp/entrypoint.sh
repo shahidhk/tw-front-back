2d0b66f121a62d9ce7d6a824a6a956277f3bd3a1
Jun Ma <jma@toronto.ca>
2019-10-25 10:38:53 -0400

    update environ vars

--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -2,7 +2,8 @@
 
 cd /app/tw-webapp
 touch /app/tw-webapp/.env.local
-printf "VUE_APP_API=eDfGfj041tHBYkX9" > /app/tw-webapp/.env.local
+printf "VUE_APP_API=eDfGfj041tHBYkX9\n" > /app/tw-webapp/.env.local
+printf "VUE_APP_URL=$VUE_APP_URL\n" > /app/tw-webapp/.env.local
 git pull
 npm install
 npm run build
