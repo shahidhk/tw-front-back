5e66bab3df2a13fd2371a6f548c021f32d02e355
Jun Ma <jma@toronto.ca>
2019-10-30 11:16:27 -0400

    update: git script for web client

--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -4,6 +4,7 @@ cd /app/tw-webapp
 touch /app/tw-webapp/.env.local
 printf "VUE_APP_API=eDfGfj041tHBYkX9\n" >> /app/tw-webapp/.env.local
 printf "VUE_APP_URL=$VUE_APP_URL\n" >> /app/tw-webapp/.env.local
-git pull
+git fetch --all
+git reset --hard origin/master
 npm install
 npm run build
